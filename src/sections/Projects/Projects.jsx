0754e78fe38ec046ad1a5c1b42b31cd3b2bb5655
mahmud handevi <93486297+mahmoudHidowi@users.noreply.github.com>
2024-06-29 00:12:27 +0300

    Add files via upload

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,42 @@
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/project-1.png';
+import freshBurger from '../../assets/project-2.png';
+import hipsster from '../../assets/project-3.png';
+import fitLift from '../../assets/9.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://ofuq-oqa5.vercel.app/about.html"
+          h3="project-1"
+          p="Academy"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://mahmoudhidowi.github.io/-web-design/"
+          h3="project-2"
+          p="Restaurant"
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/mahmoudHidowi/ecommerce-website"
+          h3="project-3"
+          p="ecommerce-website"
+        />
+        <ProjectCard
+          src={fitLift}
+          link="https://mh-plum.vercel.app/"
+          h3="project-4"
+          p="website"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
